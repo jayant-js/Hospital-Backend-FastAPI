8db635aaa9e7b25362d9e2c5aaa63a47690274f9
Jayant Singhal <jayantsinghal992@gmail.com>
2025-06-06 21:00:05 +0530

    That's all for today

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
-from fastapi import FastAPI, HTTPException, Path, Query
+from fastapi import FastAPI, HTTPException, Path, Query, Depends
+from fastapi.responses import JSONResponse
+from contextlib import asynccontextmanager
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from pydantic import BaseModel
 from typing import Annotated
-import models  
+import pymysql
+from models import *
 
 app = FastAPI()
 
-URL_DATABASE = ''
-engine = create_engine(URL_DATABASE)
+URL_DATABASE = 'mysql+pymysql://root:abhi@localhost:3306/hospital_db'
+engine = create_engine(URL_DATABASE, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    create_db_and_tables() 
+    yield
 
 @app.get("/introduction")
 def introduction_page():
     return {"message":"this is a backend project for patients database for a hospital"}
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+@app.get("/get_patient_data/{patient_id}")
+def get_patient_data(patient_id:int=Path(description="Give the Patient ID of the Patient", example=1), session:Session = Depends(get_session)):
+    patient = session.get(Patient, patient_id)
+    if not patient:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    return patient.model_dump()
+
+@app.post("/insert_new_patient")
+def insert_patient(patient:Patient, session: Session = Depends(get_session)):
+    patient.bmi = patient.weight/(patient.height**2)
+    # insert this SQLModel patient into the database
+    session.add(patient)
+    session.commit()
+    session.refresh(patient)
+    return JSONResponse(status_code=201, content="Patient Successfully Added")
--- models.py
+++ models.py
@@ -4,48 +4,45 @@ from datetime import time, date
 from pydantic import computed_field
 
 class Patient(SQLModel, table=True):
-    patient_id: Annotated[int, Field(..., primary_key=True, description="Provide the patient ID")]
+    patient_id: Annotated[int, Field(default=None, primary_key=True, description="Provide the patient ID", index=True)]
     name: Annotated[str, Field(..., description="Please give the patient name")]
     age: Annotated[int, Field(..., description="Please provide the age of the patient", gt=0, le=120)]
     email: Annotated[Optional[str], Field(default=None, description="Please provide the patient's mail id")]
     contactNo: Annotated[str, Field(..., description="Please provide the patient's contact number", min_length=10, max_length=10)]
     height: Annotated[float, Field(..., description="Please give the patient's height in meters")]
     weight: Annotated[float, Field(..., description="Please give the patient's weight in Kgs")]
-
-    @computed_field
-    def bmi(self):
-        return self.weight/(self.height*self.height)
+    bmi: Annotated[Optional[float], Field(default=None, description="The BMI is automatically calculated")]
 
 class Doctor(SQLModel, table=True):
-    doctor_id: Annotated[int, Field(..., primary_key=True,description="Please provide the doctor's ID")]
+    doctor_id: Annotated[int, Field(default=None, primary_key=True,description="Please provide the doctor's ID")]
     name: Annotated[str, Field(..., description="Please give the doctor's name")]
     age: Annotated[int, Field(..., description="Please provide the age of the doctor", gt=0, le=120)]
     email: Annotated[Optional[str], Field(default=None, description="Please provide the doctor's mail id")]
     contactNo: Annotated[str, Field(..., description="Please provide the doctor's contact number", min_length=10, max_length=10)]
 
 class Consultation(SQLModel, table=True):
-    id: Annotated[Optional[int], Field(default=None)]
+    consultation_id: Annotated[Optional[int], Field(default=None, primary_key=True)]
     patient_id: Annotated[int, Field(..., foreign_key="patient.patient_id")]
     doctor_id: Annotated[int, Field(..., foreign_key="doctor.doctor_id")]
     patientFeedback: Annotated[int, Field(..., description="Give the feedback about the doctor on the scale of 1-5", ge=0, le=5)]
 
 class Specialization(SQLModel, table=True):
-    specialization_id: Annotated[int, Field(..., primary_key=True)]
+    specialization_id: Annotated[int, Field(default=None, primary_key=True)]
     specialization: Annotated[str, Field(..., description="Please specify the specialization")]
 
 class DoctorSpecialization(SQLModel, table=True):
-    doctor_id: Annotated[int, Field(..., foreign_key="doctor.doctor_id")]
-    specialization_id: Annotated[int, Field(..., description="")]
+    doctor_id: Annotated[int, Field(..., foreign_key="doctor.doctor_id", primary_key=True)]
+    specialization_id: Annotated[int, Field(..., primary_key=True)]
 
 class TimeSlot(SQLModel, table = True):
-    timeslot_id: Annotated[int, Field(..., primary_key=True)]
+    timeslot_id: Annotated[int, Field(default=None, primary_key=True)]
     start_time: Annotated[time, Field(..., description="please provide the start time for the time slot")]
     end_time: Annotated[time, Field(..., description="please provide the end time for the time slot")]
 
 class DoctorTimeSlot(SQLModel, table = True):
-    doctor_id: Annotated[int, Field(..., foreign_key="doctor.doctor_id", description="Give the doctor id")]
-    timeslot_id: Annotated[int, Field(..., foreign_key="timeslot.timeslot_id")]
-    available_date: Annotated[date, Field(..., description="Give the date on which date the doctor is available")]
+    doctor_id: Annotated[int, Field(..., foreign_key="doctor.doctor_id", description="Give the doctor id", primary_key=True)]
+    timeslot_id: Annotated[int, Field(..., foreign_key="timeslot.timeslot_id", primary_key=True)]
+    available_date: Annotated[date, Field(..., description="Give the date on which date the doctor is available", primary_key=True)]
     booking_count: Annotated[int, Field(...)]
 
 class Appointment(SQLModel, table=True):
